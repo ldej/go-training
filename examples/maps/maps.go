9640625f243ab053485ccfdc8be9766df519d348
Laurence de Jong <info@ldej.nl>
2021-05-17 16:42:19 +0530

    Update 2021

--- examples/maps/maps.go
+++ examples/maps/maps.go
@@ -6,16 +6,16 @@ import (
 
 func main() {
 	studentsOnSchool := map[string][]string{
-		"Cambridge": []string{"Raj", "Alice"},
-		"MIT":       []string{"Bob"},
+		"Cambridge": {"Raj", "Alice"},
+		"MIT":       {"Bob"},
 	}
-	fmt.Printf("1: %+v\n", studentsOnSchool)   // %+v debugging convenience
+	fmt.Printf("1: %+v\n", studentsOnSchool) // %+v debugging convenience
 
-	studentsOnSchool["DU"] = []string{"Abhi"}         // add map entry
-	fmt.Printf("2: %+v\n", studentsOnSchool)   // %+v debugging convenience
+	studentsOnSchool["DU"] = []string{"Abhi"} // add map entry
+	fmt.Printf("2: %+v\n", studentsOnSchool)  // %+v debugging convenience
 
-	delete(studentsOnSchool, "MIT")               // remove map entry
-	fmt.Printf("3: %+v\n", studentsOnSchool)   // %+v debugging convenience
+	delete(studentsOnSchool, "MIT")          // remove map entry
+	fmt.Printf("3: %+v\n", studentsOnSchool) // %+v debugging convenience
 
 	cambridgeStudents, found := studentsOnSchool["Cambridge"] // get map entry
 	if !found {
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/ldej/go-training
 
-go 1.13
+go 1.16
 
 require (
 	github.com/golang/mock v1.4.0
